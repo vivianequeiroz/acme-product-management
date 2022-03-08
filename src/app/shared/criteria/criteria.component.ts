7ddf153ab3a4a16f0f78b4be8070888c19ae6e06
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-03-08 18:52:19 -0300

    feat: add a criteria component to use it as a child component

---
+++ src/app/shared/criteria/criteria.component.ts
@@ -0,0 +1,25 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+
+@Component({
+  selector: "pm-criteria",
+  templateUrl: "./criteria.component.html",
+  styleUrls: ["./criteria.component.css"],
+})
+export class CriteriaComponent implements OnInit, AfterViewInit {
+  @ViewChild("filterElement") filterElementRef: ElementRef;
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    if (this.filterElementRef) {
+      this.filterElementRef.nativeElement.focus();
+    }
+  }
+
+  ngOnInit() {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { StarComponent } from './star.component';
+import { StarComponent } from "./star.component";
+import { CriteriaComponent } from "./criteria/criteria.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  declarations: [
-    StarComponent
-  ],
-  exports: [
-    StarComponent,
-    CommonModule,
-    FormsModule
-  ]
+  imports: [CommonModule, FormsModule],
+  declarations: [StarComponent, CriteriaComponent],
+  exports: [StarComponent, CommonModule, FormsModule, CriteriaComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
