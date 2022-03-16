bbc0971247080f0ebe104137f7659510c0b8a017
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-03-16 19:11:29 -0300

    feat: keeping state in sync between two components

--- src/app/products/product-shell/product-shell-detail.component.ts
+++ src/app/products/product-shell/product-shell-detail.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
-import { IProduct } from '../product';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
+import { IProduct } from "../product";
 
 @Component({
-    selector: 'pm-product-shell-detail',
-    templateUrl: './product-shell-detail.component.html'
+  selector: "pm-product-shell-detail",
+  templateUrl: "./product-shell-detail.component.html",
 })
 export class ProductShellDetailComponent implements OnInit {
-    pageTitle: string = 'Product Detail';
+  pageTitle: string = "Product Detail";
 
-    constructor(private productService: ProductService) { }
+  // a simple property declaration would be only called once, at the time component was rendered
+  // so it would not keep sync between components
+  get product(): IProduct | null {
+    return this.productService.currentProduct;
+  }
 
-    ngOnInit() {
-    }
+  constructor(private productService: ProductService) {}
 
+  ngOnInit() {}
 }
