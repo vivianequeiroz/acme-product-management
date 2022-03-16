6822e87e6f1ddc0dc9a30422fcbd1468fe0df360
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-03-16 19:00:25 -0300

    chore: update root path to ProductShellComponent in order to sync state between list component and detail component

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,38 +1,40 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from "../shared/shared.module";
 
-import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { ProductEditComponent } from './edit/product-edit.component';
+import { ProductListComponent } from "./product-list.component";
+import { ProductDetailComponent } from "./product-detail.component";
+import { ProductEditComponent } from "./edit/product-edit.component";
 
-import { ProductService } from './product.service';
-import { ProductEditGuard } from './edit/product-edit-guard.service';
-import { ProductParameterService } from './product-parameter.service';
+import { ProductService } from "./product.service";
+import { ProductEditGuard } from "./edit/product-edit-guard.service";
+import { ProductParameterService } from "./product-parameter.service";
+import { ProductShellDetailComponent } from "./product-shell/product-shell-detail.component";
+import { ProductShellListComponent } from "./product-shell/product-shell-list.component";
+import { ProductShellComponent } from "./product-shell/product-shell.component";
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: '', component: ProductListComponent },
-      { path: ':id', component: ProductDetailComponent },
+      { path: "", component: ProductShellComponent },
+      { path: ":id", component: ProductDetailComponent },
       {
-        path: ':id/edit',
-        canDeactivate: [ ProductEditGuard ],
-        component: ProductEditComponent
-      }
-    ])
+        path: ":id/edit",
+        canDeactivate: [ProductEditGuard],
+        component: ProductEditComponent,
+      },
+    ]),
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductShellComponent,
+    ProductShellListComponent,
+    ProductShellDetailComponent,
   ],
-  providers: [
-    ProductService,
-    ProductEditGuard,
-    ProductParameterService
-  ]
+  providers: [ProductService, ProductEditGuard, ProductParameterService],
 })
-export class ProductModule { }
+export class ProductModule {}
