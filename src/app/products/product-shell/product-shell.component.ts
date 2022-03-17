3c45227708111a1bf12f527ea461825c45e32aa6
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-03-17 20:41:57 -0300

    feat: add additional subscription

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
 
 @Component({
-    templateUrl: './product-shell.component.html'
+  templateUrl: "./product-shell.component.html",
 })
 export class ProductShellComponent implements OnInit {
-    pageTitle: string = 'Products';
-    monthCount: number;
+  pageTitle: string = "Products";
+  monthCount: number;
 
-    constructor() { }
+  constructor(private productService: ProductService) {}
 
-    ngOnInit() {
-    }
-
+  ngOnInit() {
+    this.productService.selectedProductChanges$.subscribe((selectedProduct) => {
+      if (selectedProduct) {
+        const start = new Date(selectedProduct.releaseDate);
+        const now = new Date();
+        this.monthCount =
+          now.getMonth() -
+          start.getMonth() +
+          12 * (now.getFullYear() - start.getFullYear());
+      } else {
+        this.monthCount = 0;
+      }
+    });
+  }
 }
